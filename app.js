f35d067ae95465edac8970c1c6cd9059c5b52075
NuttySalmon <nuttysalmon@gmail.com>
2018-05-03 22:10:48 -0700

    Updated and installed newer version of Bootstrap to 4.1.1

--- app.js
+++ app.js
@@ -22,8 +22,14 @@ var commentRoutes    = require("./routes/comments"),
     
 mongoose.connect("mongodb://localhost/yelp_media_profiles");
 app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use('/', express.static(__dirname + '/www')); // redirect root
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 app.use(methodOverride('_method'));
 app.use(cookieParser('secret'));
 //require moment
