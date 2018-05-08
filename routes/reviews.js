adb42ac83463bd996309a1da258318e4fe26570e
NuttySalmon <nuttysalmon@gmail.com>
2018-05-07 20:05:23 -0700

    Implimented check existing review

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,64 +1,78 @@
 var express = require("express");
-var router  = express.Router({mergeParams: true});
+var router = express.Router({ mergeParams: true });
 var Entry = require("../models/entry");
 var Review = require("../models/review");
 var middleware = require("../middleware");
 
 //reviews New
-router.get("/new", middleware.isLoggedIn, function(req, res){
+router.get("/new", middleware.isLoggedIn, function(req, res) {
     // find entry by id
     console.log(req.params.id);
-    Entry.findById(req.params.id, function(err, entry){
-        if(err){
+    Entry.findById(req.params.id, function(err, entry) {
+        if (err) {
             console.log(err);
         } else {
-             res.render("reviews/new", {entry: entry});
+            res.render("reviews/new", { entry: entry });
         }
     })
 });
 
 //reviews Create
-router.post("/",middleware.isLoggedIn,function(req, res){
-   //lookup entry using ID
-   Entry.findById(req.params.id, function(err, entry){
-       if(err){
-           console.log(err);
-           res.redirect("/entries/search");
-       } else {
-        var newReview={
-          rating: req.body.rating,
-          comment: req.body.comment,
-          entry_id:req.params.id
-        }
+router.post("/", middleware.isLoggedIn, function(req, res) {
+    //lookup entry using ID
+    Review.find({ entry_id: req.params.id, "author.id": req.user._id })
+        .exec(function(err, review) {
+            console.log(review);
+            if (err) {
+                console.log(err);
+                res.redirect("/entries/search");
+            } else if (review.length !== 0) {
+                req.flash("error", "Cannot post review because You have already reviewed this.");
+                return res.redirect("/entries/display/" + req.params.id);
+            } else {
+                Entry.findById(req.params.id, function(err, entry) {
+                    if (err) {
+                        console.log(err);
+                        return res.redirect("/entries/search");
+                    } else {
+                        var newReview = {
+                            rating: req.body.rating,
+                            comment: req.body.comment,
+                            entry_id: req.params.id
+                        }
 
-        Review.create(newReview, function(err, review){
-           if(err){
-               console.log(err);
-           } else {
-               //add username and id to review
-               review.author.id = req.user._id;
-               review.author.username = req.user.username;
-               //save review
-               review.save();
-               console.log(entry._id);
-               entry.reviewList.push(review);
-               entry.save();
-               console.log(review);
-               req.flash('success', 'Created a review!');
-               return res.redirect('/entries/display/' + entry._id);
-           }
+                        Review.create(newReview, function(err, review) {
+                            if (err) {
+                                console.log(err);
+                            } else {
+                                //add username and id to review
+                                review.author.id = req.user._id;
+                                review.author.username = req.user.username;
+                                //save review
+                                review.save();
+                                console.log(entry._id);
+                                entry.reviewList.push(review);
+                                entry.save();
+                                console.log(review);
+                                req.flash('success', 'Created a review!');
+                                return res.redirect('/entries/display/' + entry._id);
+                            }
+                        });
+                    }
+                });
+            }
         });
-       }
-   });
+
+
 });
 
-router.get("/:reviewId/edit", middleware.isLoggedIn, function(req, res){
+router.get("/:reviewId/edit", middleware.isLoggedIn, function(req, res) {
     // find entry by id
-    Review.findById(req.params.reviewId, function(err, review){
-        if(err){
+    Review.findById(req.params.reviewId, function(err, review) {
+        if (err) {
             console.log(err);
         } else {
-             res.render("review/edit", {entry_id: req.params.id, review: review});
+            res.render("review/edit", { entry_id: req.params.id, review: review });
         }
     })
 });
